67fa7fb7abd0a76b23eb23ef75153757bd8df6c8
debian <tl@zasmlabs.com>
2024-10-19 17:23:44 +0530

    added stripe

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { api } from "@/lib/api";
+import stripePromise from "@/lib/stripe";
 
 export default function Home() {
+  const handleUpgrade = async () => {
+    try {
+      const response = await api.get("/payments/create-checkout-session");
+      const stripe = await stripePromise;
+      await stripe?.redirectToCheckout({
+        sessionId: response.data.sessionId,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      hello
-    </div>
+    <h1>
+      <Button onClick={handleUpgrade} variant={"outline"}>
+        Upgrade to Premium
+      </Button>
+    </h1>
   );
 }
